d67b125642bc3ab4267027a9f53d2b454d06f041
rainfallnz <tim.kerr@rainfall.nz>
2021-01-21 08:51:19 +1300

    trivial re-indenting

--- CallModels/General_Run_MultibandReliability.R
+++ CallModels/General_Run_MultibandReliability.R
@@ -5,26 +5,26 @@ rm(list=ls()) # clear memory
 
 #Define directories
 {
-ProjectDirectory    <- "D:\\Projects\\Aqualinc\\projects\\MPI_WaterStorage\\LimSim"  #This is for Rainfall.NZ. Edit to match local directory structure
-DataDirectory       <- file.path(ProjectDirectory,"Datasets")                        #Assumes all data is in "Datasets" sub directory of the project directory
-RFunctionsDirectory <- file.path(ProjectDirectory, "Functions")                      #Assumes all additional R files are in a "Functions" sub directory of the project directory
-FlowAllocationFile  <- file.path(ProjectDirectory,"..\\FlaxbourneExample\\WaterTakeLimits_FlaxbourneExample.csv")
+  ProjectDirectory    <- "D:\\Projects\\Aqualinc\\projects\\MPI_WaterStorage\\LimSim"  #This is for Rainfall.NZ. Edit to match local directory structure
+  DataDirectory       <- file.path(ProjectDirectory,"Datasets")                        #Assumes all data is in "Datasets" sub directory of the project directory
+  RFunctionsDirectory <- file.path(ProjectDirectory, "Functions")                      #Assumes all additional R files are in a "Functions" sub directory of the project directory
+  FlowAllocationFile  <- file.path(ProjectDirectory,"..\\FlaxbourneExample\\WaterTakeLimits_FlaxbourneExample.csv")
 }
 
 # Load functions
 {
-source(file.path(RFunctionsDirectory,"Flow&AbstractionFunctions.R"))   #This file has the "freq.restrict.multiband" function in it, needed to calculate take reliability
+  source(file.path(RFunctionsDirectory,"Flow&AbstractionFunctions.R"))   #This file has the "freq.restrict.multiband" function in it, needed to calculate take reliability
 }
 
 # Load general data
 {
-load(file.path(DataDirectory,"RECdata_9.RData")) #This is the "master" RECV1 data frame (called MyREC) complete with additional attributes including the flow duration curve parameters
-
-#load the Groundwater data from the data directory. This loads an data frame of Aquifer parameters for each REC reach.
-load(file.path(DataDirectory,"RECGroundwater.RData"))
-
-#Combine all the attributes together
-MyREC <- merge(MyREC,RECGroundwater)
+  load(file.path(DataDirectory,"RECdata_9.RData")) #This is the "master" RECV1 data frame (called MyREC) complete with additional attributes including the flow duration curve parameters
+  
+  #load the Groundwater data from the data directory. This loads an data frame of Aquifer parameters for each REC reach.
+  load(file.path(DataDirectory,"RECGroundwater.RData"))
+  
+  #Combine all the attributes together
+  MyREC <- merge(MyREC,RECGroundwater)
 }
 
 # # Set site-specific data
@@ -42,33 +42,33 @@ MyREC <- merge(MyREC,RECGroundwater)
 
 # Read in allocation file and convert to a list of relevant parameters for each site
 {
-AllocationData <- read.csv(FlowAllocationFile)
-
+  AllocationData <- read.csv(FlowAllocationFile)
+  
   # Cut down to just the columns of interest
-AllocationData <- AllocationData[,c("SiteName","RECV1ReachID","MinQ","AllocQ","AllocShare")]
-
-#Split on SiteName into a list of data frames
-SplitDataFrames <- split(AllocationData[2:5],AllocationData$SiteName)
-
-#Convert each site's data frame into a list and remove the duplicate REC SiteID's
-AllSites <- lapply(SplitDataFrames, function(x) {
-  x <- as.list(x)
-  x$RECV1ReachID <- unique(x$RECV1ReachID)
-  return(x)
-})
+  AllocationData <- AllocationData[,c("SiteName","RECV1ReachID","MinQ","AllocQ","AllocShare")]
+  
+  #Split on SiteName into a list of data frames
+  SplitDataFrames <- split(AllocationData[2:5],AllocationData$SiteName)
+  
+  #Convert each site's data frame into a list and remove the duplicate REC SiteID's
+  AllSites <- lapply(SplitDataFrames, function(x) {
+    x <- as.list(x)
+    x$RECV1ReachID <- unique(x$RECV1ReachID)
+    return(x)
+  })
 }
 
 # Find the reliability for each monitoring site. 
 Reliabilities <- lapply(seq_along(AllSites), function(SiteIndex){
   SiteName <- names(AllSites)[[SiteIndex]]
   EachSite <- AllSites[[SiteIndex]]
-  SiteREC<-MyREC[match(EachSite[["RECV1ReachID"]], MyREC$NZReach),]
+  SiteREC  <- MyREC[match(EachSite[["RECV1ReachID"]], MyREC$NZReach),]
   
   Reliability <- freq.restrict.multiband(ThisNZReach = EachSite[["RECV1ReachID"]],
-                          minFlow=EachSite[["MinQ"]],
-                          allocation=EachSite[["AllocQ"]],
-                          allocation_share=EachSite[["AllocShare"]],
-                          Data = SiteREC, FDCPlot = TRUE,SiteName = SiteName)
+                                         minFlow=EachSite[["MinQ"]],
+                                         allocation=EachSite[["AllocQ"]],
+                                         allocation_share=EachSite[["AllocShare"]],
+                                         Data = SiteREC, FDCPlot = TRUE,SiteName = SiteName)
   return(Reliability)
 })
 
